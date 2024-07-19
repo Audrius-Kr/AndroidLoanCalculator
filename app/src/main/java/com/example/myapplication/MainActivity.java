41316b1a0946edcef88f8cff9d2271eee2e1cd6a
Audrius-Kr <kralikas.aud@gmail.com>
2024-07-19 20:18:39 +0300

    Improved calculation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -19,7 +21,22 @@ public class MainActivity extends AppCompatActivity {
         // Access views using binding
         binding.loanAmountInput.setText("5000");
         binding.calculateButton.setOnClickListener(v -> {
-            // Handle button click
+            Locale locale = Locale.US;
+            int loanAmount = Integer.parseInt(binding.loanAmountInput.getText().toString());
+            int loanDuration = Integer.parseInt((binding.loanDurationInput.getText().toString())) * 12 + Integer.parseInt(binding.loanTermInMonthsInput.getText().toString());
+            double loanInterestRate = Double.parseDouble(binding.interestRateInput.getText().toString()) / 100;
+            double loanNumerator = loanAmount * (loanInterestRate / 12) * Math.pow((1 + loanInterestRate/12), loanDuration);
+            double loanDenumerator = Math.pow(1+loanInterestRate/12, loanDuration) - 1;
+            double emi = loanNumerator / loanDenumerator;
+            String monthlyPayment = String.format(locale,"%.2f", loanNumerator/loanDenumerator);
+            binding.monthlyPaymentAmount.setText(monthlyPayment);
+            double principal = emi * (Math.pow((1 + loanInterestRate / 12), loanDuration) - 1)
+                            / (Math.pow(1+loanInterestRate/12, loanDuration))
+                            / (loanInterestRate/12);
+            double totalPayed = emi * loanDuration;
+            binding.totalAmount.setText(String.format(locale,"%.2f", totalPayed));
+            binding.totalInterestAmount.setText(String.format(locale,"%.2f", totalPayed - principal));
+
         });
     }
 
